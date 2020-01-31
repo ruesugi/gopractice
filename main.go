cac5d251a7ea41822577c394ab190c4648e703c0
github RenUesugi <r_uesugi@audio-technica.co.jp>
2020-01-31 17:13:41 +0900

    [Fix] gotrade
    Change import path from gotrading to gopractice.

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
-	"gotrading/config"
-	"gotrading/utils"
+	"gopractice/config"
+	"gopractice/utils"
 	"log"
 )
 
 func main() {
 	utils.LoggingSettings(config.Config.LogFile)
-	log.Println("test")
+	log.Println(config.Config.LogFile)
+	log.Println(config.Config.Port)
+	log.Println(config.Config.Name)
+	log.Println(config.Config.Driver)
 }
